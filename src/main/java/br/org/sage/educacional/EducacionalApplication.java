1a107ed185905fb08665e6e0bacf29d8796161a9
Jorge Vilaça <jorge.vilaca@gmail.com>
2018-10-29 12:17:07 -0300

    Closes #1 projeto básico adicionado

---
+++ src/main/java/br/org/sage/educacional/EducacionalApplication.java
@@ -0,0 +1,12 @@
+package br.org.sage.educacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducacionalApplication.class, args);
+	}
+}
